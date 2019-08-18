fc151cede57b7e9d205a78593c003cbafa773e67
Dejan Belic <dejan@Dejans-MacBook-Pro.local>
2019-08-18 11:58:53 +0200

    Created store, rootReducer, action, action types - constants.

---
+++ src/js/reducers/index.js
@@ -0,0 +1,24 @@
+import { ADD_ARTICLE } from '../constants/action-types';
+
+const initialState = {
+    articles: []
+}
+
+function rootReducer (state = initialState, action) {
+
+    switch(action.type) {
+
+        case ADD_ARTICLE: {
+            return [
+                ...state,
+                {
+                    articles: state.articles.concat(action.payload)
+                }
+            ]
+        }
+        default:
+            return state;
+    }
+}
+
+export default rootReducer;
